8970667b4aa22a8afa74c586178297eb4ed279e0
Jacob Forbes <forjake23@gmail.com>
2025-09-01 17:50:53 -0500

    formatting updates

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.paywall-btn').forEach((button) => {
+document.querySelectorAll('.deafult-btn').forEach((button) => {
   button.addEventListener('click', function() {
     this.classList.add('active'); // Change button appearance (optional)
   });
