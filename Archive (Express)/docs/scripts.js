d14b9038b1d91372d1f8d8bc9dac25f0464dad5a
Jacob Forbes <forjake23@gmail.com>
2025-09-14 18:34:32 -0500

    removed old express files

--- Archive (Express)/docs/scripts.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.deafult-btn').forEach((button) => {
-  button.addEventListener('click', function() {
-    this.classList.add('active'); // Change button appearance (optional)
-  });
-});
